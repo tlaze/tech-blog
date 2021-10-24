0021e1d7ae4da6deb33396a6a6b42e998d0d448b
tlaze <tomlazore@gmail.com>
2021-10-24 15:54:45 -0400

    exampleData seeds connected to all.handlebars and displays info (posts)

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -3,14 +3,17 @@ const { Example } = require('../models/index');
 
 router.get('/', async (req, res) => {
     try{
+        const dbExampleData = await Example.findAll();
+
+        const serializedData = dbExampleData.map((data) => data.get({ plain: true }));
+        console.log('data',serializedData);
+        
         //renders all.handlebars
-        res.render('all'); 
-        // const dbExampleData = await Example.findAll();
-        // res.status(200).json(dbExampleData);
+        res.render('all', { serializedData }); 
     }
     catch(err){
         res.status(500).json(err);
     }
-})
+});
 
 module.exports = router;
