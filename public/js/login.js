37c89d1419a5bf757dcf0b8095e50e257212323e
tlaze <tomlazore@gmail.com>
2021-10-25 11:58:25 -0400

    Updated dashboard page to ask user to log in if they aren't already. Updated CSS

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,17 @@
-const loginFormHandler = async () => {
-    
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+
     const username = document.querySelector('#inputUsername').value.trim();
     const password = document.querySelector('#inputPassword').value.trim();
     if(username && password) {
-        const response = await fetch('/users', {
+        const response = await fetch('/api/users/login', {
             method: 'POST',
             body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
-            document.location.replaces('/dashboard');
+            document.location.replaces('/');
         }
         else{
             alert('Failed To Log In');
