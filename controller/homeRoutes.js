3d8314bfa6b9219bc0ea633986e50d805897b267
tlaze <tomlazore@gmail.com>
2021-10-26 23:44:06 -0400

    Posts created are added to the dashboard with the most recent on top. Posts Display all user/post information. Trying to get the welcome message to say the user's name afterward. Doesn't use the same variable as the one in the posts

--- controller/homeRoutes.js
+++ controller/homeRoutes.js
@@ -4,23 +4,21 @@ const withAuth = require('../utils/auth');
 
 // Displays all posts on homepage
 router.get('/', async (req, res) => {
-    try{
-        const postsData = await Posts.findAll({
-            include: [
-                {
-                    model: Users,
-                    attributes: ['username'],
-                },
-            ]
-        });
-        // Serialized post data so it can be read by the handlebars template
-        const posts = postsData.map((post) => post.get({ plain: true }));
-    
-        //renders homepage.handlebars
-        res.render('homepage', { 
-            posts,
-            loggedIn: req.session.loggedIn,
-        }); 
+
+  try{
+      const postsData = await Posts.findAll({
+          include: [
+              {
+                  model: Users,
+                  attributes: ['username'],
+              },
+          ]
+      });
+      // Serialized post data so it can be read by the handlebars template
+      const posts = postsData.map((post) => post.get({ plain: true }));
+  
+      //renders homepage.handlebars
+      res.render('homepage', { posts,loggedIn: req.session.loggedIn }); 
     }
     catch(err){
         console.error(err);
@@ -31,7 +29,7 @@ router.get('/', async (req, res) => {
 // If user is logged in, directs to dashboard, else goes to log in screen
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect('/dashboard');
+    res.redirect('/');
     return;
   }
   res.render('login');
@@ -42,49 +40,80 @@ router.get('/signup', async (req,res) => {
   res.render('signup');
 });
 
-
-// Displays dashboard after being logged in
+// Renders the dashboard page and displays all of the user's posts
 router.get('/dashboard', withAuth, async (req, res) => {
-  
   try{
-    // Find the logged in user based on the session ID
-    const postsData = await Posts.findAll(
-      {
-        where: {
-          user_id: req.session.user_id,
-        },
-        attributes: [
-          'id',
-          'title',
-          'description',
-          'date_created'
-        ],
-        include:{
+    const postsData = await Posts.findAll({
+    
+      where: {
+        user_id: req.session.user_id
+      },
+      order:[
+        ['date_created','DESC']
+      ],
+      attributes: [
+        'id',
+        'title',
+        'description',
+        'date_created'
+      ],
+      include: [
+        {
           model: Users,
           attributes: ['username']
-          }
-      });
-      if(!postsData){
-        res.status(404).json({ message: "No Posts Available" });
-        return;
-      }
-      const posts = postsData.map(post => post.get({ plain: true }));
-      // console.log("full dash", dashPosts.req.session);
-      posts.user = req.session.username;
-      posts.id = req.session.user_id;
-      console.log("post", posts);
-      res.render('dashboard', {
-            posts,
-            // user: req.session.username,
-            // id: req.session.user_id,
-            loggedIn: true
-      });
+        }
+      ]
+    })
+    const posts = postsData.map(post => post.get({ plain: true }));
+    console.log("posts from homeRoutes", posts);
+    res.render('dashboard', { posts, loggedIn: true });
   }
   catch(err) {
     console.error(err);
     res.status(500).json(err);
   }
 });
+
+
+// // Adds Posts data to the dashboard.
+// router.get('/dashboard', withAuth, async (req, res) => {
+  
+//   try{
+//     // Find the logged in user based on the session ID
+//     const postsData = await Posts.findAll(
+//       {
+//         where: {
+//           user_id: req.session.user_id,
+//         },
+//         attributes: [
+//           'id',
+//           'title',
+//           'description',
+//           'date_created'
+//         ],
+//         order: [
+//           ['date_created', 'DESC']
+//         ],
+//         include:{
+//           model: Users,
+//           attributes: ['username']
+//           }
+//       });
+//       if(!postsData){
+//         res.status(404).json({ message: "No Posts Available" });
+//         return;
+//       }
+//       res.json(postsData);
+//   }
+//   catch(err) {
+//     console.error(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+
+
+
 
 // Routes to new post page after clicking button
 router.get('/dashboard/new', async (req, res) => {
--- public/js/newPost.js
+++ public/js/newPost.js
@@ -16,7 +16,7 @@ const newPostHandler = async (event) => {
                 headers: { "Content-Type": "application/json" },
             });
             if(response.ok){
-                document.location.replace('../');
+                document.location.replace('/dashboard');
             }
             else{
                 alert(response.statusText);
